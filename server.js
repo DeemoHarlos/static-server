038b4bac738963e7096ab7e195b8855d9fee8c68
DeemoHarlos <cces4415@gmail.com>
2017-10-03 15:04:08 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const argv = require('minimist')(process.argv.slice(2))
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const log = require('./logger')
+const static = require('./static')
+const port = argv.p || 80
+
+log.printLog('info','Starting server ...')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.enable('trust proxy')
+
+app.use((req,res,next)=>{
+	log.listenResEnd(req,res)
+	next()
+},static('../'))
+
+app.listen(port, ()=>{
+	log.printLog('info','Listening on port ' + (port+'').cyan)
+})
